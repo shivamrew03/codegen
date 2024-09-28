b3e9f298babcc5d0a0a7bb98032b254aa65cf355
Mohammed Atif Khan <saskhan1089@gmail.com>
2024-09-28 20:06:38 +0530

    added strictquery in database.js, handled user without a token gracefully

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
   try {
+    mongoose.set('strictQuery', true);
     mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -3,9 +3,12 @@ import { UnauthorizedError } from '../utils/errors.js';
 
 const authMiddleware = (req, res, next) => {
   try {
+    console.log(req.cookies)
     const token = req.cookies?.token;
     if (!token) {
-      throw new UnauthorizedError('Authentication required');
+      // next(error)
+      return;
+      // throw new UnauthorizedError('Authentication required');
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
